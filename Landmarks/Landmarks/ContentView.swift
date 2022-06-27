d5bed0423b7037e13c9c4e3b54470156872a21c7
hwangJi-dev <melon2228@swu.ac.kr>
2022-06-28 01:47:39 +0900

    📝[Study]: #1 - Creating and Combining Views

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by hwangJi on 2022/06/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hi! I'm Borabong!!")
+            .padding()
+    }
+}
+
+// MARK: - ContentView_Previews
+// 컨텐츠 미리보기 provider
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by hwangJi on 2022/06/27.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
